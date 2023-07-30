759cf5fa7a9fd8d240469b59fdbc632ee74153b1
earlgrey02 <san06036@naver.com>
2023-07-30 13:27:56 +0900

    Feat: UserProducer correctAnswer 및 incorrectAnswer가 이벤트 객체를 전달하도록 변경

--- src/main/kotlin/com/youquiz/quiz/adapter/producer/UserProducer.kt
+++ src/main/kotlin/com/youquiz/quiz/adapter/producer/UserProducer.kt
@@ -1,17 +1,21 @@
 package com.youquiz.quiz.adapter.producer
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.youquiz.quiz.event.CorrectAnswerEvent
+import com.youquiz.quiz.event.IncorrectAnswerEvent
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 @Component
 class UserProducer(
-    private val kafkaTemplate: KafkaTemplate<String, String>
+    private val kafkaTemplate: KafkaTemplate<String, String>,
+    private val objectMapper: ObjectMapper
 ) {
-    fun correctAnswer(userId: Long) {
-        kafkaTemplate.send("correctAnswer", userId.toString())
+    fun correctAnswer(correctAnswerEvent: CorrectAnswerEvent) {
+        kafkaTemplate.send("correct-answer", objectMapper.writeValueAsString(correctAnswerEvent))
     }
 
-    fun incorrectAnswer(userId: Long) {
-        kafkaTemplate.send("incorrectAnswer", userId.toString())
+    fun incorrectAnswer(incorrectAnswerEvent: IncorrectAnswerEvent) {
+        kafkaTemplate.send("incorrect-answer", objectMapper.writeValueAsString(incorrectAnswerEvent))
     }
 }
