db8b8faa4fcf378d2e96b4410aeb215dd9a3f92e
earlgrey02 <san06036@naver.com>
2023-07-30 13:11:55 +0900

    Feat: FindUserByIdResponse 필드 추가

--- src/main/kotlin/com/youquiz/quiz/dto/FindUserByIdResponse.kt
+++ src/main/kotlin/com/youquiz/quiz/dto/FindUserByIdResponse.kt
@@ -1,6 +1,15 @@
 package com.youquiz.quiz.dto
 
-class FindUserByIdResponse(
-    val id: Long,
-    val nickname: String
+import java.time.LocalDateTime
+
+data class FindUserByIdResponse(
+    val id: String,
+    val username: String,
+    val nickname: String,
+    val role: String,
+    val allowPush: Boolean,
+    val createdDate: LocalDateTime,
+    val correctQuizIds: Set<String>,
+    val incorrectQuizIds: Set<String>,
+    val likedQuizIds: Set<String>
 )
