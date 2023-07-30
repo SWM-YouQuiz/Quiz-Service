44e3c05b795c2e1fea31e9ec284f0e65e5385616
earlgrey02 <san06036@naver.com>
2023-07-30 13:15:42 +0900

    Feat: UserClient findById의 userId를 String 타입으로 변경

--- src/main/kotlin/com/youquiz/quiz/adapter/client/UserClient.kt
+++ src/main/kotlin/com/youquiz/quiz/adapter/client/UserClient.kt
@@ -2,7 +2,7 @@ package com.youquiz.quiz.adapter.client
 
 import com.youquiz.quiz.dto.FindUserByIdResponse
 import com.youquiz.quiz.exception.UserNotFoundException
-import com.youquiz.quiz.global.userServiceClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
@@ -12,7 +12,7 @@ import org.springframework.web.reactive.function.client.awaitBody
 class UserClient(
     private val webClient: WebClient
 ) {
-    suspend fun findById(userId: Long) =
+    suspend fun findById(userId: String) =
         webClient.get()
             .uri("$userServiceClient/user/{id}", userId)
             .retrieve()
