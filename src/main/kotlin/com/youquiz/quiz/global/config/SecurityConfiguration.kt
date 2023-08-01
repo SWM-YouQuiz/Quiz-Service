40f1ffb5cb3037cf289acecf181cfe52b276cf06
earlgrey02 <san06036@naver.com>
2023-08-01 21:50:03 +0900

    Feat: Chapter CRUD 관련 권한 설정

--- src/main/kotlin/com/youquiz/quiz/global/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/youquiz/quiz/global/config/SecurityConfiguration.kt
@@ -4,7 +4,9 @@ import com.github.jwt.authentication.JwtAuthenticationFilter
 import com.github.jwt.core.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
@@ -13,6 +15,7 @@ import org.springframework.security.web.server.authentication.HttpStatusServerEn
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
 
 @Configuration
+@EnableReactiveMethodSecurity
 @EnableWebFluxSecurity
 class SecurityConfiguration {
     @Bean
@@ -24,7 +27,13 @@ class SecurityConfiguration {
             httpBasic { it.authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)) }
             securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
             authorizeExchange {
-                it.anyExchange()
+                it.pathMatchers(HttpMethod.POST, "/api/chapter/**")
+                    .hasAuthority("ADMIN")
+                    .pathMatchers(HttpMethod.PUT, "/api/chapter/**")
+                    .hasAuthority("ADMIN")
+                    .pathMatchers(HttpMethod.DELETE, "/api/chapter/**")
+                    .hasAuthority("ADMIN")
+                    .anyExchange()
                     .authenticated()
             }
             addFilterAt(JwtAuthenticationFilter(jwtProvider), SecurityWebFiltersOrder.AUTHORIZATION)
