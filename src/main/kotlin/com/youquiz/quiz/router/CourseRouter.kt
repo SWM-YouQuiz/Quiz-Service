b8a1c9f5f797ebb7a2136588308c896f3f1c04a8
earlgrey02 <san06036@naver.com>
2023-08-02 21:01:42 +0900

    Feat: CourseController CRUD 구현

---
+++ src/main/kotlin/com/youquiz/quiz/router/CourseRouter.kt
@@ -0,0 +1,20 @@
+package com.youquiz.quiz.router
+
+import com.youquiz.quiz.handler.CourseHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+class CourseRouter {
+    @Bean
+    fun courseRoutes(handler: CourseHandler) =
+        coRouter {
+            "/api/course".nest {
+                GET("", handler::getCourses)
+                POST("", handler::createCourse)
+                PUT("/{id}", handler::updateCourseById)
+                DELETE("/{id}", handler::deleteCourseById)
+            }
+        }
+}
