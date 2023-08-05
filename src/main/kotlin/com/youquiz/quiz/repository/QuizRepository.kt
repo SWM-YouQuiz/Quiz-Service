7b18737fb588345a99c334898a7c63ab41faa5cb
earlgrey02 <san06036@naver.com>
2023-08-06 01:30:03 +0900

    Feat: QuizRepository findAllByChapterId 페이징 쿼리 메소드 구현

--- src/main/kotlin/com/youquiz/quiz/repository/QuizRepository.kt
+++ src/main/kotlin/com/youquiz/quiz/repository/QuizRepository.kt
@@ -2,12 +2,15 @@ package com.youquiz.quiz.repository
 
 import com.youquiz.quiz.domain.Quiz
 import kotlinx.coroutines.flow.Flow
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface QuizRepository : CoroutineCrudRepository<Quiz, String> {
     fun findAllByChapterId(chapterId: String): Flow<Quiz>
+
+    fun findAllByChapterId(chapterId: String, pageable: Pageable): Flow<Quiz>
 
     fun findAllByWriterId(writerId: String): Flow<Quiz>
 
