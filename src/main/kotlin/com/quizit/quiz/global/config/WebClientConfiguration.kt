5dc68ebd4da5a62f656b01952adf5e91cdc715ce
earlgrey02 <san06036@naver.com>
2023-08-06 21:59:32 +0900

    Refactor: WebClientConfiguration 디렉토리 위치 수정

--- src/main/kotlin/com/quizit/quiz/global/config/WebClientConfiguration.kt
+++ src/main/kotlin/com/quizit/quiz/global/config/WebClientConfiguration.kt
@@ -1,4 +1,4 @@
-package com.quizit.quiz.global
+package com.quizit.quiz.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
