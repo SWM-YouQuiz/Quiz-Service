555b1022f64f83634036cfce5cbc592a011bc3e2
earlgrey02 <san06036@naver.com>
2023-08-28 18:40:18 +0900

    Feat: Curriculum CRUD 관련 fixture 추가

---
+++ src/test/kotlin/com/quizit/quiz/fixture/CurriculumFixture.kt
@@ -0,0 +1,47 @@
+package com.quizit.quiz.fixture
+
+import com.quizit.quiz.domain.Curriculum
+import com.quizit.quiz.dto.request.CreateCurriculumRequest
+import com.quizit.quiz.dto.request.UpdateCurriculumByIdRequest
+import com.quizit.quiz.dto.response.CurriculumResponse
+
+fun createCreateCurriculumRequest(
+    title: String = TITLE,
+    image: String = IMAGE
+): CreateCurriculumRequest =
+    CreateCurriculumRequest(
+        title = title,
+        image = image
+    )
+
+fun createUpdateCurriculumByIdRequest(
+    id: String = ID,
+    title: String = TITLE,
+    image: String = IMAGE
+): UpdateCurriculumByIdRequest =
+    UpdateCurriculumByIdRequest(
+        title = title,
+        image = image
+    )
+
+fun createCurriculumResponse(
+    id: String = ID,
+    title: String = TITLE,
+    image: String = IMAGE
+): CurriculumResponse =
+    CurriculumResponse(
+        id = id,
+        title = title,
+        image = image
+    )
+
+fun createCurriculum(
+    id: String = ID,
+    title: String = TITLE,
+    image: String = IMAGE
+): Curriculum =
+    Curriculum(
+        id = id,
+        title = title,
+        image = image
+    )
