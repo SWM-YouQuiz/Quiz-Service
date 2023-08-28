366f5eac6a15018bcda1f3cce284d9b85ea69319
earlgrey02 <san06036@naver.com>
2023-08-28 18:43:50 +0900

    Feat: CurriculumController CRUD 구현

---
+++ src/main/kotlin/com/quizit/quiz/handler/CurriculumHandler.kt
@@ -0,0 +1,36 @@
+package com.quizit.quiz.handler
+
+import com.quizit.quiz.dto.request.CreateCurriculumRequest
+import com.quizit.quiz.dto.request.UpdateCurriculumByIdRequest
+import com.quizit.quiz.service.CurriculumService
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.*
+
+@Component
+class CurriculumHandler(
+    private val curriculumService: CurriculumService
+) {
+    suspend fun getCurriculums(request: ServerRequest): ServerResponse =
+        ServerResponse.ok().bodyAndAwait(curriculumService.getCurriculums())
+
+    suspend fun createCurriculum(request: ServerRequest): ServerResponse =
+        request.awaitBody<CreateCurriculumRequest>().let {
+            ServerResponse.ok().bodyValueAndAwait(curriculumService.createCurriculum(it))
+        }
+
+    suspend fun updateCurriculumById(request: ServerRequest): ServerResponse =
+        with(request) {
+            val id = pathVariable("id")
+            val updateCurriculumByIdRequest = awaitBody<UpdateCurriculumByIdRequest>()
+
+            ServerResponse.ok()
+                .bodyValueAndAwait(curriculumService.updateCurriculumById(id, updateCurriculumByIdRequest))
+        }
+
+    suspend fun deleteCurriculumById(request: ServerRequest): ServerResponse =
+        request.pathVariable("id").let {
+            curriculumService.deleteCurriculumById(it)
+
+            ServerResponse.ok().buildAndAwait()
+        }
+}
---
+++ src/main/kotlin/com/quizit/quiz/router/CurriculumRouter.kt
@@ -0,0 +1,24 @@
+package com.quizit.quiz.router
+
+import com.quizit.quiz.handler.CurriculumHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+class CurriculumRouter {
+    @Bean
+    fun curriculumRoutes(handler: CurriculumHandler): RouterFunction<ServerResponse> =
+        coRouter {
+            "/curriculum".nest {
+                GET("", handler::getCurriculums)
+            }
+            "/admin/curriculum".nest {
+                POST("", handler::createCurriculum)
+                PUT("/{id}", handler::updateCurriculumById)
+                DELETE("/{id}", handler::deleteCurriculumById)
+            }
+        }
+}
