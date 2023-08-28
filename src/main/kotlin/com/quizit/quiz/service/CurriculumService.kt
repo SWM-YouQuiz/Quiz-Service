a362742ce0f20a1401d602fe06b0c9b9a1472a48
earlgrey02 <san06036@naver.com>
2023-08-28 18:40:31 +0900

    Feat: CurriculumService CRUD 구현

---
+++ src/main/kotlin/com/quizit/quiz/service/CurriculumService.kt
@@ -0,0 +1,49 @@
+package com.quizit.quiz.service
+
+import com.quizit.quiz.domain.Curriculum
+import com.quizit.quiz.dto.request.CreateCurriculumRequest
+import com.quizit.quiz.dto.request.UpdateCurriculumByIdRequest
+import com.quizit.quiz.dto.response.CurriculumResponse
+import com.quizit.quiz.exception.CurriculumNotFoundException
+import com.quizit.quiz.repository.CurriculumRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.springframework.stereotype.Service
+
+@Service
+class CurriculumService(
+    private val curriculumRepository: CurriculumRepository
+) {
+    fun getCurriculums(): Flow<CurriculumResponse> =
+        curriculumRepository.findAll()
+            .map { CurriculumResponse(it) }
+
+    suspend fun createCurriculum(request: CreateCurriculumRequest): CurriculumResponse =
+        with(request) {
+            curriculumRepository.save(
+                Curriculum(
+                    title = title,
+                    image = image
+                )
+            ).let { CurriculumResponse(it) }
+        }
+
+    suspend fun updateCurriculumById(id: String, request: UpdateCurriculumByIdRequest): CurriculumResponse =
+        with(request) {
+            curriculumRepository.findById(id)?.let {
+                curriculumRepository.save(
+                    Curriculum(
+                        id = id,
+                        title = title,
+                        image = image
+                    )
+                )
+            }?.let {
+                CurriculumResponse(it)
+            } ?: throw CurriculumNotFoundException()
+        }
+
+    suspend fun deleteCurriculumById(id: String) {
+        curriculumRepository.deleteById(id)
+    }
+}
