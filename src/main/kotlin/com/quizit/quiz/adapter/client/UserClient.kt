7eb9effc21051fe6731b291ae2ecd501832c0bf4
earlgrey02 <san06036@naver.com>
2023-09-03 14:04:01 +0900

    Feat: UserClient 외부 서비스 통신에 인가 과정 추가

--- src/main/kotlin/com/quizit/quiz/adapter/client/UserClient.kt
+++ src/main/kotlin/com/quizit/quiz/adapter/client/UserClient.kt
@@ -2,7 +2,9 @@ package com.quizit.quiz.adapter.client
 
 import com.quizit.quiz.dto.response.GetUserByIdResponse
 import com.quizit.quiz.exception.UserNotFoundException
+import com.quizit.quiz.global.config.getCurrentAuthentication
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
@@ -17,6 +19,7 @@ class UserClient(
     suspend fun getUserById(userId: String) =
         webClient.get()
             .uri("$url/api/user/user/{id}", userId)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer ${getCurrentAuthentication().token}")
             .retrieve()
             .onStatus(HttpStatus.NOT_FOUND::equals) { throw UserNotFoundException() }
             .awaitBody<GetUserByIdResponse>()
