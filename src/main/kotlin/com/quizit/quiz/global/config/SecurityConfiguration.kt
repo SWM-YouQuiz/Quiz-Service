d8e1b609598187fd6abb14d2a0087b2ed08a9121
earlgrey02 <san06036@naver.com>
2023-09-05 00:08:07 +0900

    Feat: Actuator 엔드포인트 권한 허용 설정

--- src/main/kotlin/com/quizit/quiz/global/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/quizit/quiz/global/config/SecurityConfiguration.kt
@@ -32,6 +32,8 @@ class SecurityConfiguration {
             authorizeExchange {
                 it.pathMatchers("/quiz/admin/**")
                     .hasAuthority("ADMIN")
+                    .pathMatchers("/actuator/health/**")
+                    .permitAll()
                     .anyExchange()
                     .authenticated()
             }
