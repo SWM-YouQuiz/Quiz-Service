1f15c01b1a0099c4d5c319034768f67a7800fb5e
earlgrey02 <san06036@naver.com>
2023-09-19 10:42:53 +0900

    Feat: CommonFixture objectMapper 추가

--- src/test/kotlin/com/quizit/quiz/fixture/CommonFixture.kt
+++ src/test/kotlin/com/quizit/quiz/fixture/CommonFixture.kt
@@ -1,8 +1,14 @@
 package com.quizit.quiz.fixture
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import org.springframework.data.domain.PageRequest
 import java.time.LocalDateTime
 
 const val ID = "test"
 val CREATED_DATE = LocalDateTime.now()!!
 val PAGEABLE = PageRequest.of(0, 3)
+val objectMapper = ObjectMapper().apply {
+    registerModules(JavaTimeModule(), kotlinModule())
+}
