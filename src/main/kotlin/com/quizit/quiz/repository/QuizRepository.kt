af604020f4cd282da30f9f5c06570d3850107620
earlgrey02 <san06036@naver.com>
2023-09-28 19:51:59 +0900

    Fix: getQuizzesByChapterIdAndAnswerRateRange에서 정답률 필터가 미만, 초과로 작동하던 버그 수정

--- src/main/kotlin/com/quizit/quiz/repository/QuizRepository.kt
+++ src/main/kotlin/com/quizit/quiz/repository/QuizRepository.kt
@@ -2,12 +2,14 @@ package com.quizit.quiz.repository
 
 import com.quizit.quiz.domain.Quiz
 import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 
 @Repository
 interface QuizRepository : ReactiveMongoRepository<Quiz, String> {
+    @Query("{'chapterId': ?0,'answerRate': {'\$gte': ?1, '\$lte': ?2 } }")
     fun findAllByChapterIdAndAnswerRateBetween(
         chapterId: String, minAnswerRate: Double, maxAnswerRate: Double, pageable: Pageable
     ): Flux<Quiz>
