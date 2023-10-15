80cf4c69e43841699617ea7ac6af200b971df170
earlgrey02 <san06036@naver.com>
2023-10-15 14:43:38 +0900

    Feat: QuizRepository 커리큘럼, 코스, 챕터 별 퀴즈 조회 기능 구현

--- src/main/kotlin/com/quizit/quiz/repository/QuizRepository.kt
+++ src/main/kotlin/com/quizit/quiz/repository/QuizRepository.kt
@@ -2,6 +2,7 @@ package com.quizit.quiz.repository
 
 import com.quizit.quiz.domain.Quiz
 import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
@@ -9,6 +10,8 @@ import reactor.core.publisher.Flux
 
 @Repository
 interface QuizRepository : ReactiveMongoRepository<Quiz, String> {
+    fun findAllByChapterId(chapterId: String): Flux<Quiz>
+
     @Query("{'chapterId': ?0,'answerRate': {'\$gte': ?1, '\$lte': ?2 } }")
     fun findAllByChapterIdAndAnswerRateBetween(
         chapterId: String, minAnswerRate: Double, maxAnswerRate: Double, pageable: Pageable
@@ -19,4 +22,28 @@ interface QuizRepository : ReactiveMongoRepository<Quiz, String> {
     fun findAllByIdIn(ids: List<String>): Flux<Quiz>
 
     fun findAllByQuestionContains(keyword: String): Flux<Quiz>
+
+    @Aggregation(
+        pipeline = [
+            "{ \$match: { '_id': ?0 } }",
+            "{ \$lookup: { from: 'course', localField: '_id', foreignField: 'curriculumId', as: 'courses' } }",
+            "{ \$unwind: '\$courses' }",
+            "{ \$lookup: { from: 'chapter', localField: 'courses._id', foreignField: 'courseId', as: 'chapters' } }",
+            "{ \$unwind: '\$chapters' }",
+            "{ \$lookup: { from: 'quiz', localField: 'chapters._id', foreignField: 'chapterId', as: 'quizzes' } }",
+            "{ \$unwind: '\$quizzes' }",
+        ]
+    )
+    fun findAllByCurriculumId(curriculumId: String): Flux<Quiz>
+
+    @Aggregation(
+        pipeline = [
+            "{ \$match: { '_id': ?0 } }",
+            "{ \$lookup: { from: 'chapter', localField: '_id', foreignField: 'courseId', as: 'chapters' } }",
+            "{ \$unwind: '\$chapters' }",
+            "{ \$lookup: { from: 'quiz', localField: 'chapters._id', foreignField: 'chapterId', as: 'quizzes' } }",
+            "{ \$unwind: '\$quizzes' }",
+        ]
+    )
+    fun findAllByCourseId(courseId: String): Flux<Quiz>
 }
