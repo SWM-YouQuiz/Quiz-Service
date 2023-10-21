519cb10f40673d839cf8fff52884eaa49db1476a
earlgrey02 <san06036@naver.com>
2023-10-21 20:59:40 +0900

    Feat: QuizProducer의 모든 producer가 Mono<Void>를 반환하도록 수정

--- src/main/kotlin/com/quizit/quiz/adapter/producer/QuizProducer.kt
+++ src/main/kotlin/com/quizit/quiz/adapter/producer/QuizProducer.kt
@@ -3,21 +3,27 @@ package com.quizit.quiz.adapter.producer
 import com.quizit.quiz.dto.event.CheckAnswerEvent
 import com.quizit.quiz.dto.event.DeleteQuizEvent
 import com.quizit.quiz.dto.event.MarkQuizEvent
+import com.quizit.quiz.global.aop.annotation.Producer
 import com.quizit.quiz.global.config.producerLogging
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
-import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
-import reactor.kafka.sender.SenderResult
 
-@Component
+@Producer
 class QuizProducer(
     private val kafkaTemplate: ReactiveKafkaProducerTemplate<String, Any>,
 ) {
-    fun deleteQuiz(event: DeleteQuizEvent): Mono<SenderResult<Void>> =
+    fun deleteQuiz(event: DeleteQuizEvent): Mono<Void> =
         kafkaTemplate.send("delete-quiz", event)
             .doOnNext { producerLogging(event) }
+            .then()
 
-    fun markQuiz(event: MarkQuizEvent): Mono<SenderResult<Void>> =
+    fun checkAnswer(event: CheckAnswerEvent): Mono<Void> =
+        kafkaTemplate.send("check-answer", event)
+            .doOnNext { producerLogging(event) }
+            .then()
+
+    fun markQuiz(event: MarkQuizEvent): Mono<Void> =
         kafkaTemplate.send("mark-quiz", event)
             .doOnNext { producerLogging(event) }
+            .then()
 }
