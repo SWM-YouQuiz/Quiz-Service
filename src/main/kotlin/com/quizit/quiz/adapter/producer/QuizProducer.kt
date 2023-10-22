35eb07133a2bcf916128062a2b54807f18a86e47
earlgrey02 <san06036@naver.com>
2023-10-23 04:21:23 +0900

    Feat: QuizProducer 및 QuizConsumer에 로깅 AOP 적용

--- src/main/kotlin/com/quizit/quiz/adapter/producer/QuizProducer.kt
+++ src/main/kotlin/com/quizit/quiz/adapter/producer/QuizProducer.kt
@@ -3,8 +3,7 @@ package com.quizit.quiz.adapter.producer
 import com.quizit.quiz.dto.event.CheckAnswerEvent
 import com.quizit.quiz.dto.event.DeleteQuizEvent
 import com.quizit.quiz.dto.event.MarkQuizEvent
-import com.quizit.quiz.global.aop.annotation.Producer
-import com.quizit.quiz.global.config.producerLogging
+import com.quizit.quiz.global.annotation.Producer
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import reactor.core.publisher.Mono
 
@@ -14,16 +13,13 @@ class QuizProducer(
 ) {
     fun deleteQuiz(event: DeleteQuizEvent): Mono<Void> =
         kafkaTemplate.send("delete-quiz", event)
-            .doOnNext { producerLogging(event) }
             .then()
 
     fun checkAnswer(event: CheckAnswerEvent): Mono<Void> =
         kafkaTemplate.send("check-answer", event)
-            .doOnNext { producerLogging(event) }
             .then()
 
     fun markQuiz(event: MarkQuizEvent): Mono<Void> =
         kafkaTemplate.send("mark-quiz", event)
-            .doOnNext { producerLogging(event) }
             .then()
 }
