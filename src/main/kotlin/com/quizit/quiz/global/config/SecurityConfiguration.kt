e5ea86ecea83f40fba96970ef44d9904925e45a8
earlgrey02 <san06036@naver.com>
2023-10-23 04:22:00 +0900

    Refactor: Configuration 클래스 리팩토링

--- src/main/kotlin/com/quizit/quiz/global/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/quizit/quiz/global/config/SecurityConfiguration.kt
@@ -1,6 +1,5 @@
 package com.quizit.quiz.global.config
 
-import com.github.jwt.authentication.DefaultJwtAuthentication
 import com.github.jwt.authentication.JwtAuthenticationFilter
 import com.github.jwt.core.JwtProvider
 import com.quizit.quiz.domain.enum.Role
@@ -10,18 +9,17 @@ import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
-import org.springframework.web.reactive.function.server.ServerRequest
-import reactor.core.publisher.Mono
 
-@Configuration
 @EnableWebFluxSecurity
+@Configuration
 class SecurityConfiguration {
     @Bean
-    fun filterChain(http: ServerHttpSecurity, jwtProvider: JwtProvider): SecurityWebFilterChain =
+    fun filterChain(
+        http: ServerHttpSecurity, jwtAuthenticationFilter: JwtAuthenticationFilter
+    ): SecurityWebFilterChain =
         with(http) {
             csrf { it.disable() }
             formLogin { it.disable() }
@@ -38,13 +36,11 @@ class SecurityConfiguration {
                     .anyExchange()
                     .authenticated()
             }
-            addFilterAt(JwtAuthenticationFilter(jwtProvider), SecurityWebFiltersOrder.AUTHORIZATION)
+            addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHORIZATION)
             build()
         }
+
+    @Bean
+    fun jwtAuthenticationFilter(jwtProvider: JwtProvider): JwtAuthenticationFilter =
+        JwtAuthenticationFilter(jwtProvider)
 }
-
-fun ServerRequest.authentication(): Mono<DefaultJwtAuthentication> =
-    principal().cast(DefaultJwtAuthentication::class.java)
-
-fun DefaultJwtAuthentication.isAdmin(): Boolean =
-    isAuthenticated && (authorities[0] == SimpleGrantedAuthority(Role.ADMIN.name))
